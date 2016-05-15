614f7ee5a9211984d14afc4b05317e611b7b40e6
Dogan Ersoz <dersoz@gmail.com>
2016-05-15 12:43:54 +0300

    Sherlock is done

--- src/algorithms/implementations/SherlockAndTheBeast.scala
+++ src/algorithms/implementations/SherlockAndTheBeast.scala
@@ -5,6 +5,10 @@ package algorithms.implementations
   */
 object SherlockAndTheBeast {
 
+  val INVALID_STR = "-1"
+  val FIVE = "5"
+  val THREE = "3"
+
   def main(args: Array[String]) {
     val sc = new java.util.Scanner(System.in)
     val t = sc.nextInt()
@@ -18,22 +22,20 @@ object SherlockAndTheBeast {
   }
 
   def largestDecentNumber(numOfDigits: Int): String = {
+    if (numOfDigits <= 0)
+      return INVALID_STR
     var reminder = numOfDigits
-    var iter = 1
-    if (reminder % 15 == 0) "5" * reminder
-    else if (reminder % 5 == 0) "3" * reminder
-    else if (reminder % 3 == 0) "5" * reminder
-    else {
-      do {
-        reminder -= 5
-        if (reminder % 3 == 0) {
-          return "5" * reminder + "3" * iter * 3
-        }
-        iter += 1
-      } while (reminder > 0)
-      // Or else ...
-      "-1"
+    if (reminder % 15 == 0 || reminder % 3 == 0) {
+      return FIVE * reminder
     }
+    do {
+      reminder -= 5
+      if (reminder >= 0 && reminder % 3 == 0) {
+        return FIVE * reminder + THREE * (numOfDigits - reminder)
+      }
+    } while (reminder > 0)
+    // Or else ...
+    INVALID_STR
   }
 
 }
