8b54f29931f5217f900af84f78f1fea3876c8f6a
Dogan Ersoz <dersoz@gmail.com>
2016-08-29 11:46:58 +0300

    Basic array solution

---
+++ src/ds/arrays/ArraysDs.scala
@@ -0,0 +1,23 @@
+package ds.arrays
+
+/**
+  * Created by dogan on 29/08/16.
+  */
+object ArraysDs {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextInt()
+    val arr = new Array[Int](n)
+    for (arr_i <- 0 until n) {
+      arr(arr_i) = sc.nextInt()
+    }
+
+    printArray(arr)
+  }
+
+  def printArray[T](a: Array[T]): Unit = {
+    a.reverse.foreach(x => print(x + " "))
+  }
+
+}
